e9e08c1fc3b4e0c48b7c9db003ac7e701f928c58
sagarsetru <sagarsetru@gmail.com>
2020-09-27 21:23:43 -0400

    draft of code for how each news organization should populate the drop down menu. now need to remove old code for charities and then implement this per news org

--- src/browser_action/browser_action.js
+++ src/browser_action/browser_action.js
@@ -12,14 +12,93 @@ chrome.runtime.onMessage.addListener((msg, sender) => {
 
   chrome.runtime.onMessage.addListener((msg, sender) => {
   // First, validate the message's structure.
-    if (msg.subject === 'sendCharities') {
+    if (msg.subject === 'sendArticles') {
         
 
-		console.log('charities recieved')
+		console.log('articles recieved')
 		console.log(msg)
 
          // Get the event_list container
-        let charity_list = document.getElementById("charity_list");
+        let article_list = document.getElementById("article_list");
+
+        // get the json output
+        let articles_json = msg.message
+
+        console.log(articles_json)
+
+        let abcnews = articles_json['abcnews.go.com']
+
+        var elmnt = document.createElement("div")
+        elmnt.setAttribute("class", "newsOrg");
+
+        // set the news org info
+        var news_name = 'abcnews';
+        var news_link = abcnews;
+
+        console.log(news_name)
+        console.log(news_link)
+
+        // Change the header text
+        document.getElementsByClassName('default')[0].innerHTML  = "Consider reading related articles from these news sources.";
+
+        // Conatiner for name + dropdown
+        var name = document.createElement("button");
+        name.innerHTML = news_name;
+        name.setAttribute("class", "name");
+
+        // create a link to the relevant search
+        var link = document.createElement("div");
+        link.setAttribute("class", "link");
+
+        var link_ref = document.createElement("a");
+        // Open a blank tab when the link is clicked.
+        link_ref.innerHTML = news_name;
+        link_ref.setAttribute("target", "_blank");
+        link_ref.setAttribute("href", 'https://'.concat(news_link));
+
+        link.appendChild(link_ref)
+
+        // Put the score and link to the element
+        elmnt.appendChild(name);
+        elmnt.appendChild(link);
+
+        // Append the new element to the list.
+        article_list.appendChild(elmnt);     
+
+        let aljazeera = articles_json['aljazeera.com']
+        let apnews = articles_json['apnews.com']
+        let bbc = articles_json['bbc.com']
+        let bloomberg = articles_json['bloomberg.com']
+        let breitbart = articles_json['breitbart.com']
+        let buzzfeednews = articles_json['buzzfeednews.com']
+        let cbn = articles_json['cbn.com']
+        let cbsnews = articles_json['cbsnews.com']
+        let csmonitor = articles_json['csmonitor.com']
+        let cnn = articles_json['cnn.com']
+        let thedailybeast = articles_json['thedailybeast.com']
+        let democracynow = articles_json['democracynow.org']
+        let factcheck = articles_json['factcheck.org']
+        let forbes = articles_json['forbes.com']
+        let foxnews = articles_json['foxnews.com']
+        let huffpost = articles_json['huffpost.com']
+        let motherjones = articles_json['motherjones.com']
+        let msnbc = articles_json['msnbc.com']
+        let nationalreview = articles_json['nationalreview.com']
+        let nbcnews = articles_json['nbcnews.com']
+        let nypost = articles_json['nypost.com']
+        let nytimes = articles_json['nytimes.com']
+        let newsmax = articles_json['newsmax.com']
+        let npr = articles_json['npr.org']
+        let politico = articles_json['politico.com']
+        let reason = articles_json['reason.com']
+        let reuters = articles_json['reuters.com']
+        let salon = articles_json['salon.com']
+        let spectator = articles_json['spectator.org']
+        let theatlantic = articles_json['theatlantic.com']
+        let theguardian = articles_json['theguardian.com']
+        let thehill = articles_json['thehill.com']
+        let wsj = articles_json['wsj.com']
+
 
         let charities_json = msg.message
 
@@ -44,7 +123,7 @@ chrome.runtime.onMessage.addListener((msg, sender) => {
               elmnt.setAttribute("class", "charity");
 
               // Obtain the charity ifno.
-              var charity_name =names[i];
+              var charity_name = names[i];
               var charity_link = links[i];
               var charity_score = scores[i];
               var charity_subcat = subcats[i];
@@ -56,7 +135,7 @@ chrome.runtime.onMessage.addListener((msg, sender) => {
 
 
               // Change the header text
-              document.getElementsByClassName('default')[0].innerHTML  = "You might consider donating to the following charities";
+              document.getElementsByClassName('default')[0].innerHTML  = "Consider reading these articles";
 
               // Container for the charity score.
               var score = document.createElement("div");
@@ -102,14 +181,14 @@ chrome.runtime.onMessage.addListener((msg, sender) => {
               elmnt.appendChild(link);
 
               // Append the new element to the list.
-              charity_list.appendChild(elmnt);              
+              article_list.appendChild(elmnt);              
 
-                }
+            }
 
-                var acc = document.getElementsByClassName("name");
-                var i;
+            var acc = document.getElementsByClassName("name");
+            var i;
 
-                for (i = 0; i < acc.length; i++) {
+            for (i = 0; i < acc.length; i++) {
                 acc[i].addEventListener("click", function() {
                   /* Toggle between adding and removing the "active" class,
                   to highlight the button that controls the panel */
